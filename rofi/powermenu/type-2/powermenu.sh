b047247f6f587403bbc2e485cc134bf374af438f
CamargoLC <arbitrate2019@proton.me>
2026-08-03 14:02:10 -0300

    Refatoração do Rofi e remoção de bloatware

--- rofi/powermenu/type-2/powermenu.sh
+++ rofi/powermenu/type-2/powermenu.sh
@@ -50,35 +50,18 @@ confirm_exit() {
 
 # Pass variables to rofi dmenu
 run_rofi() {
-        echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+        echo -e "$lock\n$logout\n$reboot\n$shutdown" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-        selected="$(confirm_exit)"
-        if [[ "$selected" == "$yes" ]]; then
-                if [[ $1 == '--shutdown' ]]; then
-                        systemctl poweroff
-                elif [[ $1 == '--reboot' ]]; then
-                        systemctl reboot
-                elif [[ $1 == '--suspend' ]]; then
-                        mpc -q pause
-                        amixer set Master mute
-                        systemctl suspend
-                elif [[ $1 == '--logout' ]]; then
-                        if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
-                                openbox --exit
-                        elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
-                                bspc quit
-                        elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
-                                i3-msg exit
-                        elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
-                                qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-                        fi
-                fi
-        else
-                exit 0
-        fi
+    if [[ $1 == '--shutdown' ]]; then
+        alacritty -e sudo init 0
+    elif [[ $1 == '--reboot' ]]; then
+        alacritty -e sudo reboot
+    elif [[ $1 == '--logout' ]]; then
+        bspc quit
+    fi
 }
 
 # Actions
