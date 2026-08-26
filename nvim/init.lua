c7e3863d7213df6a5b536b3c7f20b1ae6a09ff65
CamargoLC <arbitrate2019@proton.me>
2026-08-26 19:55:48 -0300

    mudando tema do nvim e colocando a configuração do cava

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,24 @@
 require('config.options')
 vim.opt.guicursor = "a:ver25"
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  { "xero/miasma.nvim", lazy = false, priority = 1000 },
+})
+
+vim.cmd("colorscheme miasma")
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,4 +1,3 @@
-vim.api.nvim_set_hl(0,"Normal", {bg = "none"})
 vim.opt.cursorline = true
 vim.opt.number = true
 vim.opt.shiftwidth = 4
